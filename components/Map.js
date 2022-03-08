36b92312eb76dab592350b3840f6154f4219d29d
bhuwansingh248 <bhuwansingh248@gmail.com>
2022-03-09 01:18:02 +0530

    (+) map Component (+) LocationSelector component (*) index.js

--- components/Map.js
+++ components/Map.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import {useEffect, React} from 'react'
+import mapboxgl from 'mapbox-gl'
 
+const style={
+  wrapper:'flex-1 h-full w-full',
+
+}
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmh1d2Fuc2luZ2gyNDgiLCJhIjoiY2wwaWV4YXNpMDF1OTNkbnN1Y2l2NnZxNiJ9.4A64vrDQhpgryGDa7u2kLg'
 const Map = () => {
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container:'map',
+      style:'mapbox://styles/bhuwansingh248/cl0i5xgv1000a14rtxxbksqrk',
+      center:[79.52433, 29.21609],
+      zoom:10,
+    })
+  }, [])
   return (
-    <div>Map</div>
+
+    <div className={style.wrapper} id='map'>Map</div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import LocationSelector from "../components/LocationSelector"
 import Map from "../components/Map"
 import Navbar from "../components/Navbar"
 
 // to write multiple classes for css 
 const style = {
   wrapper : 'h-screen w-screen flex flex-col',
+  main: 'h-full w-screen flex-1 z-10',
+  mapContainer: 'flex-1 w-full h-full',
+  rideRequestContainer: 'h-full w-[400px] m1-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end z-20',
+  rideRequest: 'h-full max-h-[700px] bg-white rounded-lg flex flex-col overflow-scroll',
 }
 
 export default function Home() {
@@ -15,7 +20,7 @@ export default function Home() {
       </div>
       <div className={style.rideRequestContainer}>
         <div className={style.rideRequest}>
-        {/* location selector   */}
+        <LocationSelector/>
         {/* confirm ride */}
         </div>
       </div>
